d1c09647f7572daba159c51cb5bd83e41e286809
Priya <pdugg73@gmail.com>
2026-04-10 14:24:28 +0530

    Ready for deployement

--- app.py
+++ app.py
@@ -144,8 +144,7 @@ def search():
 
 
 # ---------------- RUN ----------------
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+    app.run()
